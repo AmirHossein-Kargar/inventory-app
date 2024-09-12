fdf3a2f8b300dfe60855b17db5f7a0db6327c494
Amir Hossein Kargar <amirkaargar@hotmail.com>
2024-09-12 13:22:42 +0330

    feat: :sparkles: getAllProducts and saveProducts method
    get all products from Local Storage and sort them! and with saveProducts method we can manage our products

--- src/js/Storage.js
+++ src/js/Storage.js
@@ -3,13 +3,13 @@ const products = [
     id: 1,
     title: "React.js",
     category: "frontend",
-    updated: "'2024-07-20T08:32:19.057Z'",
+    createdAt: "'2024-07-20T08:32:19.057Z'",
   },
   {
     id: 2,
     title: "Node.js",
     category: "backend",
-    updated: "'2023-10-10T08:32:19.057Z'",
+    createdAt: "'2023-10-10T08:32:19.057Z'",
   },
 ];
 const categories = [
@@ -29,7 +29,6 @@ const categories = [
 
 export default class Storage {
   // ? add new category
-  // ? save category
 
   // ? get all categories
   static getAllCategories() {
@@ -40,6 +39,7 @@ export default class Storage {
     });
     return sortedCategories;
   }
+  // ? save category
   static saveCategory(categoryToSave) {
     const savedCategories = Storage.getAllCategories();
     const existedItem = savedCategories.find((c) => c.id == categoryToSave.id);
@@ -49,10 +49,35 @@ export default class Storage {
       existedItem.description = categoryToSave.description;
     } else {
       // ? new
-      categoryToSave.id = new Date().getItem()
-      categoryToSave.createdAt = new Date().toISOString()
-      savedCategories.push(categoryToSave)
+      categoryToSave.id = new Date().getItem();
+      categoryToSave.createdAt = new Date().toISOString();
+      savedCategories.push(categoryToSave);
     }
     localStorage.setItem("category", JSON.stringify(savedCategories));
+  }
+  static getAllProducts() {
+    const savedProducts = JSON.parse(localStorage.getItem("products")) || [];
+
+    return savedProducts.sort((a, b) => {
+      return new Date(a.createdAt) > new Date(b.createdAt) ? -1 : 1;
+    });
+  }
+  static saveProducts(productToSave) {
+      const savedProducts = Storage.getAllProducts();
+      const existedItem = savedProducts.find(
+        (c) => c.id == productToSave.id
+      );
+      if (existedItem) {
+        // ? edit
+        existedItem.title = productToSave.title;
+        existedItem.quantity = productToSave.quantity;
+        existedItem.category = productToSave.category
+      } else {
+        // ? new
+        productToSave.id = new Date().getItem();
+        productToSave.createdAt = new Date().toISOString();
+        savedProducts.push(productToSave);
+      }
+      localStorage.setItem("products", JSON.stringify(savedProducts));
   }
 }
